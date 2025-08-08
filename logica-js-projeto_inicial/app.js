3041d091d8f097b3d0d2a3b89683ec7dce059216
laura853 <lauralopess2006@gmail.com>
2025-08-08 15:25:31 -0300

    exercicio 3

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,60 +1,89 @@
-alert("Boas vindas ao nosso site!");
+// alert("Boas vindas ao nosso site!");
 
-let nome = "Lua";
+// let nome = "Lua";
 
-let idade = 25;
+// let idade = 25;
 
-let numeroDeVendas = 50;
+// let numeroDeVendas = 50;
 
-let saldoDisponivel = 1000;
+// let saldoDisponivel = 1000;
 
-let mensagemDeErro = "Erro preencha todos os campos"
+// let mensagemDeErro = "Erro preencha todos os campos"
 
-alert (mensagemDeErro);
+// alert (mensagemDeErro);
 
-let nomeUsuario =  prompt ("Digite o seu nome"
-);
-let idadeUsuario =  prompt ("Digite sua idade");
+// let nomeUsuario =  prompt ("Digite o seu nome"
+// );
+// let idadeUsuario =  prompt ("Digite sua idade");
 
-if 
-    (idadeUsuario >= 18) {
-        alert("Pode tirar a habilitação!");
-    }
+// if 
+//     (idadeUsuario >= 18) {
+//         alert("Pode tirar a habilitação!");
+//     }
 
-console.log(saldoDisponivel);    
+// console.log(saldoDisponivel);    
 
-diaDaSemana = prompt ("Qual é o dia da semana?");
+// diaDaSemana = prompt ("Qual é o dia da semana?");
 
-if (diaDaSemana == 'Sábado') {
-    alert("Bom fim de semana");
-} else if (diaDaSemana == 'Domingo') {
-    alert("Bom fim de semana!");
-}   else {
-    alert("Boa semana!");
-} 
+// if (diaDaSemana == 'Sábado') {
+//     alert("Bom fim de semana");
+// } else if (diaDaSemana == 'Domingo') {
+//     alert("Bom fim de semana!");
+// }   else {
+//     alert("Boa semana!");
+// } 
 
 
-let numero = prompt("Digite um número");
+// let numero = prompt("Digite um número");
 
-if (numero >= 0) {
-    alert("Esse é um número positivo!");
-} else {
-    alert("Esse é um número negativo!");
+// if (numero >= 0) {
+//     alert("Esse é um número positivo!");
+// } else {
+//     alert("Esse é um número negativo!");
+// }
+
+
+// let pontuacao = 108;
+
+// if (pontuacao >= 100) {
+//     console.log("Parabéns, você ganhou!");
+// } else {
+//     console.log("Tente novamente para ganhar!");
+// }
+
+// alert(`Seu saldo disponível é : ${saldoDisponivel}`);
+
+// let usuarioNome = prompt("Digite seu nome:");
+// alert(`Boas vindas ${usuarioNome}`);
+
+
+
+// let contador = prompt("Digite o número 1");
+
+// while (contador < 11) {
+//   alert(contador++);
+// }
+
+
+// let contador2 = prompt("Digite o número 10");
+
+// while (contador2 > -1) {
+//     alert(contador2--);
+// }
+
+
+
+// let contador3 = prompt("Digite um número");
+
+// while (contador3 > -1) {
+//     console.log(contador3--);
+// }
+
+let contador4 = prompt("Digite um número");
+
+let numero = 0;
+
+while (numero <= contador4) { 
+    console.log(numero);
+    numero++;
 }
-
-
-let pontuacao = 108;
-
-if (pontuacao >= 100) {
-    console.log("Parabéns, você ganhou!");
-} else {
-    console.log("Tente novamente para ganhar!");
-}
-
-alert(`Seu saldo disponível é : ${saldoDisponivel}`);
-
-let usuarioNome = prompt("Digite seu nome:");
-alert(`Boas vindas ${usuarioNome}`);
-
-
-    
